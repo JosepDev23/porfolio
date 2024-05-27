769cea9a2c25ce2722f10bfe39cc2492f8c95577
JosepDev23 <josepdev23@gmail.com>
2024-05-27 14:59:08 +0200

    Update AboutJson.tsx

--- src/components/About/components/DynamicText/texts/AboutJson.tsx
+++ src/components/About/components/DynamicText/texts/AboutJson.tsx
@@ -6,12 +6,12 @@ const AboutJson: React.FC = () => {
     <div className="about-main">
       <p>&#123;</p>
       <p>&nbsp;&nbsp;"name": "Josep Ramon Ribera",</p>
-      <p>&nbsp;&nbsp;"languages": [</p>
+      <p>&nbsp;&nbsp;{`"languages": {`}</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;"valencian": "native",</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;"spanish": "native",</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;"english": "B2",</p>
-      <p>&nbsp;&nbsp;],</p>
-      <p>&nbsp;&nbsp;"programming-languages-and-frameworks": [</p>
+      <p>&nbsp;&nbsp;{`},`}</p>
+      <p>&nbsp;&nbsp;{`"programming-languages-and-frameworks": {`}</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;"typescript": "really-good",</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;"html": "really-good",</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;"css/sass": "really-good",</p>
@@ -23,7 +23,7 @@ const AboutJson: React.FC = () => {
       <p>&nbsp;&nbsp;&nbsp;&nbsp;"springboot": "medium",</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;"c#": "good",</p>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;"entity-framework": "medium",</p>
-      <p>&nbsp;&nbsp;],</p>
+      <p>&nbsp;&nbsp;{`},`}</p>
       <p>&nbsp;&nbsp;"soft-skills": [</p>
       <p>
         &nbsp;&nbsp;&nbsp;&nbsp;"critical-thinking","communication","adaptability",
